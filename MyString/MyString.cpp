cc8bfaaab0e6ee49b2524cb3ac6993fba445b469
DashkaKakashka12 <dashakarachun72@gmail.com>
2025-06-19 21:31:21 +0300

    Добавьте файлы проекта.

---
+++ MyString/MyString.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class MyString {
+public:
+
+	MyString() {
+		str = nullptr;
+	}
+
+	MyString(const char* str) {
+		int length = strlen(str);
+		this->str = new char[length + 1];
+		for (int i = 0; i < length; i++)
+		{
+			this->str[i] = str[i];
+		}
+
+		this->str[length] = '\0';
+
+	}
+
+	~MyString() {
+
+		delete[] this->str;
+	}
+
+	void Print() {
+		cout << "Текст: " << str << endl;
+	}
+
+	MyString& operator = (const MyString& other) {
+		if (this->str != nullptr) {
+			delete[] str;
+		}
+
+		int length = strlen(other.str);
+		this->str = new char[length + 1];
+		for (int i = 0; i < length; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+
+		this->str[length] = '\0';
+
+		return*this;
+	}
+
+	/*MyString operator +(const MyString& other) {
+	 MyString newStr;
+	 int thislenght = strlen(this.str);
+	 newStr.str = new
+	}*/
+
+private:
+	char* str;
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	MyString str("Hello");
+	MyString str2("World");
+
+	MyString result;
+	result = str + str2;
+
+	cout << "-----" << endl;
+
+	str = str2;
+
+
+
+	return 0;
+}
